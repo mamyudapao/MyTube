6696ef99fa8eba4a7f7796b1a5bff63a38d245a3
noshian kenichi <nosikenit@gmail.com>
2021-08-16 18:09:03 +0900

    feat: add a tempolary header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,12 @@
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
   return (
-    <div className="header flex">
-      <h1>MyTube</h1>
-      <FontAwesomeIcon icon={faCoffee} />
+    <div className="header flex justify-between p-4">
+      <h1 className="ml-8 text-3xl">MyTube</h1>
+      <FontAwesomeIcon icon={faSearch} className="mr-8 fa-2x" />
     </div>
   )
 }
